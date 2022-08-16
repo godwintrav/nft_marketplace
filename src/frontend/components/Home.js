082fdad5cb9d1cf71df7e09a4d1ab8955b6bab5d
Odenigbo Godwin <godwintrav@gmail.com>
2022-08-16 02:49:11 +0000

    added loadMarketplace function

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -3,6 +3,36 @@ import { ethers } from "ethers";
 import { Row, Col, Card, Button } from 'react-bootstrap';
 
 const Home = ({ marketplace, nft }) => {
+    const [items, setItems] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const loadMarketplaceItems = async () => {
+        const itemCount = await marketplace.itemCount();
+        let items = [];
+        for (let i = 1; i <= itemCount; i++) {
+            const item = await marketplace.items(i);
+            if(!item.sold){
+                //get uri url from contract
+                const uri = await nft.tokenURI(item.tokenId);
+                //use uri to fetch the nft metadata stored on ipfs
+                const response = await fetch(uri);
+                const metadata = await response.json();
+                //get total price of item (item price + fee)
+                const totalPrice = await marketplace.getTotalPrice(item.itemId);
+                //add item to items array
+                items.push({
+                    totalPrice,
+                    itemId: item.itemId,
+                    seller: item.seller,
+                    name: metadata.name,
+                    description: metadata.description,
+                    image: metadata.image
+                });
+            }
+        }
+        setItems(items);
+        setLoading(false);
+    }
     return (
         <div className="flex justify-center">
 
